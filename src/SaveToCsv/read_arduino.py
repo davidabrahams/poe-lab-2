5b6fa6a3c5603fdc6b9f4e0e502f7ccb978419cd
David Abrahams <david.abrahams@students.olin.edu>
2015-09-19 11:55:26 -0400

    fixed bugs

--- src/SaveToCsv/read_arduino.py
+++ src/SaveToCsv/read_arduino.py
@@ -3,29 +3,36 @@ import threading
 import json
 import math
 
-distances =[]
-
-running = True
-
-t = threading.Thread(target = read_arduino)
-s = raw_input('Press s to start')
-t.start()
-q = raw_input('Press q to quit')
-running = False
-ser = serial.Serial('/dev/ttyACM0', 9600)
-
+# TODO: THIS IS SO WRONG.
 def calibration(d):
-	d = 3.95*math.exp(-.06*d)
+    d = 3.95*math.exp(-.06*d)
+    return d
+
 def read_arduino():
-	while running:
-    	line = ser.readline()
-    	d = float(line)
-    	d = 
-    	distances.append(float(line))
+    while running:
+        line = ser.readline()
+        d = float(line[:4])
+        d = calibration(d)
+        print "%f inches" % d
+        distances.append(d)
 
 def save_data():
-	with open('data.txt', 'w') as outfile:
-		json.dump(distances, outfile)
+    with open('data.txt', 'w') as outfile:
+        json.dump(distances, outfile)
+
+
+if __name__ == '__main__':
+
+    running = True
+    distances =[]
+    ser = serial.Serial('/dev/ttyACM1', 9600)
+
+    t = threading.Thread(target =read_arduino)
+    s = raw_input('Press s to start! --> ')
+    t.start()
+    q = raw_input('Press q to quit! --> ')
+    running = False
+    save_data()
 
 
 
