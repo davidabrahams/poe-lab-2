20214bd98708b3f67c1625c669b9a2259f6a22e5
Brenna Manning <brenna.manning@students.olin.edu>
2015-09-19 11:36:02 -0400

    read arduino and save to file

--- src/SaveToCsv/read_arduino.py
+++ src/SaveToCsv/read_arduino.py
@@ -1,6 +1,31 @@
 import serial
+import threading
+import json
+import math
+
+distances =[]
+
+running = True
+
+t = threading.Thread(target = read_arduino)
+s = raw_input('Press s to start')
+t.start()
+q = raw_input('Press q to quit')
+running = False
 ser = serial.Serial('/dev/ttyACM0', 9600)
-while True:
-    line = ser.readline()
-    print line
+
+def calibration(d):
+	d = 3.95*math.exp(-.06*d)
+def read_arduino():
+	while running:
+    	line = ser.readline()
+    	d = float(line)
+    	d = 
+    	distances.append(float(line))
+
+def save_data():
+	with open('data.txt', 'w') as outfile:
+		json.dump(distances, outfile)
+
+
 
