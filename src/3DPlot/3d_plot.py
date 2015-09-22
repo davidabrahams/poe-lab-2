b47467d78b2684aa7b0486f22dc30da7a7123061
Brenna Manning <brenna.manning@students.olin.edu>
2015-09-22 16:31:46 -0400

    3D Plotting!

--- src/3DPlot/3d_plot.py
+++ src/3DPlot/3d_plot.py
@@ -3,6 +3,7 @@ from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import json
 import os
+from numpy import sin, cos, radians
 
 toplevel_dir = os.path.join(os.path.dirname(__file__),
     os.path.pardir,
@@ -10,10 +11,13 @@ toplevel_dir = os.path.join(os.path.dirname(__file__),
 filename = os.path.join(toplevel_dir, "data.txt")
 v_points =10
 h_points =20
-h_degrees_min = -45
-h_degrees_max = 45
-v_degrees_min = -22.5
-v_degrees_max = 22.5
+
+# Due to how our servos and sensor are oriented, at a high index the sensor
+# looks down and left
+h_degrees_min = -45 + 90
+h_degrees_max = 45 + 90
+v_degrees_min = -22.5 + 90
+v_degrees_max = 22.5 + 90
 
 
 def read_data(fn):
@@ -30,12 +34,26 @@ def get_angles(indices, anglemin, anglemax, steps):
     angle = anglemin + (indices.astype(float) / steps) * (anglemax - anglemin)
     return angle
 
-def get_cartesian(data_points):
-    pass
+def get_cartesian(h_angles, v_angles, distances):
+    h_rads = radians(h_angles)
+    v_rads = radians(v_angles)
+
+    x = distances*sin(v_rads)*cos(h_rads)
+    y = distances* sin(v_rads)*sin(h_rads)
+    z = distances*cos(v_rads)
+
+    return np.array([x, y, z]).T
 
 
-    def plot_points(x, y, z):
-    pass
+def plot_points(x, y, z):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(x, y, z)
+    ax.set_xlabel('X Label')
+    ax.set_ylabel('Y Label')
+    ax.set_zlabel('Z Label')
+    plt.show()
+
 
 
 def main():
@@ -46,7 +64,9 @@ def main():
     v_pos_servo = points[:,2]
     h_angles = get_angles(h_pos_servo, h_degrees_min, h_degrees_max, h_points)
     v_angles = get_angles(v_pos_servo, v_degrees_min, v_degrees_max, v_points)
-    print h_angles
+    cartesian= get_cartesian(h_angles, v_angles, distances)
+    x, y, z = cartesian[:, 0], cartesian[:, 1], cartesian[:, 2]
+    plot_points(x, y, z)
 
 if __name__ == '__main__':
     main()
