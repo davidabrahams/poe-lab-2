59856d363e5055f18e8ef2af9d637cda813c1dec
David Abrahams <david.abrahams@students.olin.edu>
2015-09-25 15:42:15 -0400

    angles work!

--- src/3DPlot/3d_plot.py
+++ src/3DPlot/3d_plot.py
@@ -17,35 +17,36 @@ v_degrees_max = 22.5
 
 
 def read_data(fn):
-	#takes in file name (ex: data.txt)
-	#reads scanner data
-	with open(fn) as file_obj:
-		scanner_data = json.load(file_obj) 
-	data_points=np.array(scanner_data)
+    #takes in file name (ex: data.txt)
+    #reads scanner data
+    with open(fn) as file_obj:
+        scanner_data = json.load(file_obj)
+    data_points=np.array(scanner_data)
 
-	return data_points
+    return data_points
 
 
-def get_angle(index, anglemin, anglemax, steps):
-	angle = anglemin + (float(index) / steps) * (anglemax - anglemin)
-	return angle
+def get_angles(indices, anglemin, anglemax, steps):
+    angle = anglemin + (indices.astype(float) / steps) * (anglemax - anglemin)
+    return angle
 
 def get_cartesian(data_points):
-	pass
+    pass
 
 
-def plot_points(x, y, z):
-	pass
-	
+    def plot_points(x, y, z):
+    pass
+
 
 def main():
-	#Creates 3d plot from data points
-	points = read_data(filename)
-	distances = points[:,1]
-	h_pos_servo = points[:,2]
-	v_pos_servo = points[:,3]
-	h_angles = get_angle(h_pos_servo, h_degrees_min, h_degrees_max, h_points)
-	print h_angles
+    #Creates 3d plot from data points
+    points = read_data(filename)
+    distances = points[:,0]
+    h_pos_servo = points[:,1]
+    v_pos_servo = points[:,2]
+    h_angles = get_angles(h_pos_servo, h_degrees_min, h_degrees_max, h_points)
+    v_angles = get_angles(v_pos_servo, v_degrees_min, v_degrees_max, v_points)
+    print h_angles
 
 if __name__ == '__main__':
     main()
